40878dae7547744ced2d612cdb009ec12fd68b57
AmyInoa <91210803+AmyInoa@users.noreply.github.com>
2021-09-22 13:01:19 -0400

    Add files via upload

---
+++ hello world version 2.py
@@ -0,0 +1,17 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /Users/amyinoa/Documents/assignment HWP .py =============
+Hello my name is insert your first name and last name
+>>> 
+============= RESTART: /Users/amyinoa/Documents/assignment HWP .py =============
+Hello my name is insert your Amy and Inoa
+>>> 
+>>> 
+============= RESTART: /Users/amyinoa/Documents/assignment HWP .py =============
+Hello my name is your Amy and Inoa
+>>> 
+============= RESTART: /Users/amyinoa/Documents/assignment HWP .py =============
+Amy Inoa
+>>> 
